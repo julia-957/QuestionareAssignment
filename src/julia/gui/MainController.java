80171d9ba54af85af45c409d2ee2d3b1c48164e9
Julia Ilasova <you@example.com>
2022-09-30 11:11:34 +0200

    initial commit

---
+++ src/julia/gui/MainController.java
@@ -0,0 +1,18 @@
+package julia.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class MainController {
+
+    @FXML
+    private void switchToQuestionare(){
+    }
+
+}
