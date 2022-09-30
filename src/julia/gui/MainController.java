d397fddec6bb8ecc263ded7cf26db8b679c7717e
Julia Ilasova <you@example.com>
2022-09-30 15:08:15 +0200

    created an intro screen with a name input and a button, after clicking the button, the scene changes into a questionnaire, passing the name from name input into the questionnaire doesn't work yet

--- src/julia/gui/MainController.java
+++ src/julia/gui/MainController.java
@@ -4,15 +4,48 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import julia.Main;
+
+import javax.swing.*;
+
 
 public class MainController {
 
+    private int disagreeCount;
+    private int neutralCount;
+    private int agreeCount;
+    private String participant;
+
     @FXML
-    private void switchToQuestionare(){
+    private Button fillQuestionnaire;
+    @FXML
+    private TextField nameInput;
+    @FXML
+    private Label participantName;
+
+    @FXML
+    private void switchToQuestionnaire(ActionEvent actionEvent){
+        participant = nameInput.getText();
+        Stage stage = (Stage) fillQuestionnaire.getScene().getWindow();
+        stage.setScene(julia.Main.questionnaire);
     }
 
+    @FXML
+    private void disagreeSelected(){
+
+    }
+
+    @FXML
+    private void neutralSelected(){
+
+    }
+
+    @FXML
+    private void agreeSelected(){
+
+    }
 }
